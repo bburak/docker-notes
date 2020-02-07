d6d70ef64b6acc8f137683d55e9ed97ff444ed77
bburak <burak.bilg@yandex.com>
2020-02-07 23:24:21 +0300

    Add Redis and modifying app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+from redis import Redis, RedisError
 from flask import Flask
+
+# Connect to Redis.
+redis = Redis(host="redis")
+app = Flask(__name__)
 
 app = Flask(__name__)
 
+
 # Define / route.
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>Can't connect to Redis, counter disabled</i>"
 
+    # Markup for response.
+    html = "<h3>Hello Docker!</h3>" \
+           "<b>Visits:</b> {visits}"
+
+    # Replace variables from template with actual values.
+    return html.format(visits=visits)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
